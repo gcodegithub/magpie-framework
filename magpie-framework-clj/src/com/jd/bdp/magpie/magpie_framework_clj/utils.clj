e80c5f0ebc0a3e8153f57a46fdf48d6f1b6646ff
Phoenix Zhang <pho.coderzhang@gmail.com>
2015-10-23 16:53:31 +0800

    fix

--- magpie-framework-clj/src/com/jd/bdp/magpie/magpie_framework_clj/utils.clj
+++ magpie-framework-clj/src/com/jd/bdp/magpie/magpie_framework_clj/utils.clj
@@ -56,7 +56,7 @@
 (defn get-task-command
   [task-command-node]
   (try
-    (case (magpie-utils/bytes->string (zk/get-data task-command-node))
+    (case ((magpie-utils/bytes->string (zk/get-data task-command-node)) "command")
       "init" :init
       "run" :run
       "reload" :reload
